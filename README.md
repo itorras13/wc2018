26f501df0c912d1148f2ba864765566b7611f7e9
itorras13 <itorras13@gmail.com>
2018-05-03 09:55:14 -0700

    Rules changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Euro2016
+# WC2018
 
-A website where people submit brackets prediction scores and winners of the UEFA Euro Cup 2016. Then standings are updated throughout the tournament revealing a winner. Made with Flask and using Heroku.
+A website where people submit brackets prediction scores and winners of the Fifa World Cup 2018. Then standings are updated throughout the tournament revealing a winner. Made with Flask and using Heroku.
--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 # import sys
 # print(current_stats, file=sys.stderr)
 from flask import Flask, request, render_template, redirect, url_for, flash
+from flask.ext.sqlalchemy import SQLAlchemy
 import os
 import psycopg2
 from datetime import datetime
@@ -14,24 +15,17 @@ import operator
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-# db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 # from models import Submission
+
+submissions = getSubmissions()
+subs = sorted(submissions, key=lambda d: d['points'], reverse=True)
 
 
 @app.route('/')
 def index():
-    submissions = getSubmissions()
-    subs = sorted(submissions, key=lambda d: d['points'], reverse=True)
-    return render_template(
-        'index.html',
-        submissions=addPlace(subs),
-        modal="none",
-        mdebug=print_in_console)
-
-
-def print_in_console(message):
-    print str(message)
+    return render_template('index.html', submissions=addPlace(subs))
 
 
 def addPlace(submissions):
@@ -141,28 +135,30 @@ def getSubmissions():
 # 	sub = Submission.query.filter_by(id=id).all()
 # 	return render_template('show.html', sub=sub[0])
 
-# @app.route('/submit', methods=['GET', 'POST'])
-# def submit():
-# 	if request.method == 'POST':
-# 		success = submit_quiniela(request)
-# 		if success:
-# 			message = sendgrid.Mail()
-# 			email = str(request.form['email'])
-# 			message.add_to(str(request.form['first_name']) + " " +
-# 				str(request.form['last_name']) + ' <' + email + '>')
-# 			message.add_bcc('itorras13@gmail.com')
-# 			message.set_subject('Eurocup16 Submission Confirmed')
-# 			submissions = get_submissions("show")
-# 			html = render_template('email_template.html', submissions=submissions, email=email)
-# 			message.set_html(html)
-# 			message.set_from('Ignacio Torras <itorras13@gmail.com>')
-# 			status, msg = sg.send(message)
-# 			submissions = get_submissions("index")
-# 			return render_template("index.html", submissions=submissions, modal="success")
-# 		else:
-# 			submissions = get_submissions("index")
-# 			return render_template("index.html", modal="failure", submissions=submissions)
-# 	return render_template('submit.html')
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    if request.method == 'POST':
+        success = submit_quiniela(request)
+        if success:
+            return render_template('index.html', submissions=addPlace(subs))
+
+# message = sendgrid.Mail()
+# email = str(request.form['email'])
+# message.add_to(str(request.form['first_name']) + " " +
+# 	str(request.form['last_name']) + ' <' + email + '>')
+# message.add_bcc('itorras13@gmail.com')
+# message.set_subject('Eurocup16 Submission Confirmed')
+# submissions = get_submissions("show")
+# html = render_template('email_template.html', submissions=submissions, email=email)
+# message.set_html(html)
+# message.set_from('Ignacio Torras <itorras13@gmail.com>')
+# status, msg = sg.send(message)
+# submissions = get_submissions("index")
+        else:
+            # submissions = get_submissions("index")
+            return render_template('index.html', submissions=addPlace(subs))
+    return render_template('index.html', submissions=addPlace(subs))
 
 
 @app.errorhandler(404)
@@ -252,34 +248,54 @@ def page_not_found(error):
 # 			updated_submissions.append(new_sub)
 # 		return updated_submissions
 
-# def submit_quiniela(request):
-# 	email=request.form['email']
-# 	sub_num = request.form['submission_num']
-# 	if not db.session.query(Submission).filter(Submission.submission_number == sub_num, Submission.email == email).count():
-# 		new = Submission(request.form['first_name'], request.form['last_name'], email, sub_num,
-# 			request.form["a1h"], request.form["a1a"], request.form["a2h"], request.form["a2a"], request.form["a3h"], request.form["a3a"],
-# 			request.form["a4h"], request.form["a4a"], request.form["a5h"], request.form["a5a"], request.form["a6h"], request.form["a6a"],
-# 			request.form["b1h"], request.form["b1a"], request.form["b2h"], request.form["b2a"], request.form["b3h"], request.form["b3a"],
-# 			request.form["b4h"], request.form["b4a"], request.form["b5h"], request.form["b5a"], request.form["b6h"], request.form["b6a"],
-# 			request.form["c1h"], request.form["c1a"], request.form["c2h"], request.form["c2a"], request.form["c3h"], request.form["c3a"],
-# 			request.form["c4h"], request.form["c4a"], request.form["c5h"], request.form["c5a"], request.form["c6h"], request.form["c6a"],
-# 			request.form["d1h"], request.form["d1a"], request.form["d2h"], request.form["d2a"], request.form["d3h"], request.form["d3a"],
-# 			request.form["d4h"], request.form["d4a"], request.form["d5h"], request.form["d5a"], request.form["d6h"], request.form["d6a"],
-# 			request.form["e1h"], request.form["e1a"], request.form["e2h"], request.form["e2a"], request.form["e3h"], request.form["e3a"],
-# 			request.form["e4h"], request.form["e4a"], request.form["e5h"], request.form["e5a"], request.form["e6h"], request.form["e6a"],
-# 			request.form["f1h"], request.form["f1a"], request.form["f2h"], request.form["f2a"], request.form["f3h"], request.form["f3a"],
-# 			request.form["f4h"], request.form["f4a"], request.form["f5h"], request.form["f5a"], request.form["f6h"], request.form["f6a"],
-# 			request.form["a1"], request.form["a2"], request.form["b1"], request.form["b2"], request.form["c1"], request.form["c2"],
-# 			request.form["d1"], request.form["d2"], request.form["e1"], request.form["e2"], request.form["f1"], request.form["f2"],
-# 			request.form["third1"], request.form["third2"], request.form["third3"], request.form["third4"],
-# 			request.form["q1"], request.form["q2"], request.form["q3"], request.form["q4"],
-# 			request.form["q5"], request.form["q6"], request.form["q7"], request.form["q8"],
-# 			request.form["semi1"], request.form["semi2"], request.form["semi3"], request.form["semi4"],
-# 			request.form["fin1"], request.form["fin2"], request.form["third_place"], request.form["champion"], request.form["top_scorer"])
-# 		db.session.add(new)
-# 		db.session.commit()
-# 		return True
-# 	return False
+
+def submit_quiniela(request):
+    email = request.form['email']
+    if db.session.query(Submission).filter(
+            Submission.email == email).count() < 3:
+        new = Submission(
+            request.form['first_name'], request.form['last_name'], email,
+            sub_num, request.form["a1h"], request.form["a1a"],
+            request.form["a2h"], request.form["a2a"], request.form["a3h"],
+            request.form["a3a"], request.form["a4h"], request.form["a4a"],
+            request.form["a5h"], request.form["a5a"], request.form["a6h"],
+            request.form["a6a"], request.form["b1h"], request.form["b1a"],
+            request.form["b2h"], request.form["b2a"], request.form["b3h"],
+            request.form["b3a"], request.form["b4h"], request.form["b4a"],
+            request.form["b5h"], request.form["b5a"], request.form["b6h"],
+            request.form["b6a"], request.form["c1h"], request.form["c1a"],
+            request.form["c2h"], request.form["c2a"], request.form["c3h"],
+            request.form["c3a"], request.form["c4h"], request.form["c4a"],
+            request.form["c5h"], request.form["c5a"], request.form["c6h"],
+            request.form["c6a"], request.form["d1h"], request.form["d1a"],
+            request.form["d2h"], request.form["d2a"], request.form["d3h"],
+            request.form["d3a"], request.form["d4h"], request.form["d4a"],
+            request.form["d5h"], request.form["d5a"], request.form["d6h"],
+            request.form["d6a"], request.form["e1h"], request.form["e1a"],
+            request.form["e2h"], request.form["e2a"], request.form["e3h"],
+            request.form["e3a"], request.form["e4h"], request.form["e4a"],
+            request.form["e5h"], request.form["e5a"], request.form["e6h"],
+            request.form["e6a"], request.form["f1h"], request.form["f1a"],
+            request.form["f2h"], request.form["f2a"], request.form["f3h"],
+            request.form["f3a"], request.form["f4h"], request.form["f4a"],
+            request.form["f5h"], request.form["f5a"], request.form["f6h"],
+            request.form["f6a"], request.form["a1"], request.form["a2"],
+            request.form["b1"], request.form["b2"], request.form["c1"],
+            request.form["c2"], request.form["d1"], request.form["d2"],
+            request.form["e1"], request.form["e2"], request.form["f1"],
+            request.form["f2"], request.form["third1"], request.form["third2"],
+            request.form["third3"], request.form["third4"], request.form["q1"],
+            request.form["q2"], request.form["q3"], request.form["q4"],
+            request.form["q5"], request.form["q6"], request.form["q7"],
+            request.form["q8"], request.form["semi1"], request.form["semi2"],
+            request.form["semi3"], request.form["semi4"], request.form["fin1"],
+            request.form["fin2"], request.form["third_place"],
+            request.form["champion"], request.form["top_scorer"])
+        db.session.add(new)
+        db.session.commit()
+        return True
+    return False
+
 
 if __name__ == "__main__":
     # app.debug = True
