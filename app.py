d801284645b00318649c291ef925ce2abef2f6c4
Ignacio Torras <ignacio@secondspectrum.com>
2018-07-02 08:51:47 -0700

    brazil

--- app.py
+++ app.py
@@ -38,18 +38,18 @@ def addPlace(submissions):
 
 def pointsPossible(sub):
     quarter_possibilities = [
-                             ['Brazil', 'Mexico'], ['Belgium','Japan'],  
+                             ['Belgium','Japan'],  
                              ['Sweden', 'Switzerland'], ['Colombia', 'England']]
     semi_possibilities = [['Uruguay', 'France'], 
-                            ['Brazil', 'Mexico', 'Belgium', 'Japan'], 
+                            ['Brazil', 'Belgium', 'Japan'], 
                             ['Russia', 'Croatia'],
                             ['Sweden', 'Switzerland', 'Colombia', 'England']]
     final_possibilities = [['Uruguay', 'France'
-                            ,'Brazil', 'Mexico', 'Belgium', 'Japan'], 
+                            ,'Brazil', 'Belgium', 'Japan'], 
                             ['Russia', 'Croatia',
                             'Sweden', 'Switzerland', 'Colombia', 'England']]
     all_left = ['Uruguay', 'France'
-                ,'Brazil', 'Mexico', 'Belgium', 'Japan', 
+                ,'Brazil', 'Belgium', 'Japan', 
                 'Russia', 'Croatia',
                 'Sweden', 'Switzerland', 'Colombia', 'England']
     quarters = [sub.r1, sub.r2, sub.r3, sub.r4, sub.r5, sub.r6, sub.r7, sub.r8]
